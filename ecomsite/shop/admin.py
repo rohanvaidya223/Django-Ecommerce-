4437c6ab889ab3a115fe59e1ff116fd5bb361459
Rohan Vaidya <124707639+rohanvaidya223@users.noreply.github.com>
2023-08-30 20:15:18 +0530

    added requirements

---
+++ ecomsite/shop/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Order, Products
+# Register your models here.
+admin.site.site_header="E-commerce Site"
+admin.site.site_title="ABC Shopping"
+admin.site.index_title="Manage ABC Shopping"
+
+class ProductAdmin(admin.ModelAdmin):
+    def change_category_to_default(self,request,queryset):
+        queryset.update(category="default")
+    change_category_to_default.short_description="Default Category"
+    list_display=('title','price','discount_price','category')
+    search_fields=('title',)
+    actions = ('change_category_to_default')
+    list_editable=('price','category',)
+    
+
+admin.site.register(Products,ProductAdmin)
+admin.site.register(Order)
